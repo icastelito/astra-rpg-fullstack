57c602acb13530c80588e153d0182b6767094665
bob <i.castelobp@gmail.com>
2023-02-15 22:56:56 -0300

    Ajustes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
 const express = require("express"); // Importa o express
 const app = express(); // Cria o express app
-app.listen(3000, () => console.log("Server started"));
+app.get("/magia", (req, res) => res.status(200).send("Ola mundo bom"));
+
+module.exports = app;
